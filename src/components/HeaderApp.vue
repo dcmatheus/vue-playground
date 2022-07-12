e4eb8e3cd3fa93dce3c074f2146349de25444db9
dcmatheus <mathws.costag@gmail.com>
2022-07-12 20:34:04 -0300

    Modifica o componente Header para utilizar Composition API

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,77 +1,87 @@
-<template>
-  <header class="header">{{ size }}</header>
-</template>
+<script setup lang="ts">
+import {
+  onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUnmounted, onUpdated, ref,
+} from 'vue';
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+const size = ref(`${window.innerWidth} x ${window.innerHeight}`);
+const root = ref<HTMLElement | null>(null);
 
-export default defineComponent({
-  name: 'HeaderApp',
-  data() {
-    return {
-      size: `${window.innerWidth} x ${window.innerHeight}`,
-    };
-  },
-  // Criação
-  //    Preparar o componente
-  //    Ajax, inicializar algumas variáveis
-  //    Não tem acesso ao template (DOM)
-  // Montagem
-  //    Inicializar um lib externa (new Lib())
-  //    Precisa de acesso ao template (DOM)
-  //    Tem acesso ao template (DOM)
-  // Atualização
-  //    Debug
-  //    Update
-  // Desmontagem
-  //    Remover tudo o que for ncessário (lib->destroy())
-  //    para liberar memória
+function resize($evt: Event) {
+  console.log($evt);
+  size.value = `${window.innerWidth} x ${window.innerHeight}`;
+}
 
-  beforeUpdate() {
-    console.log('beforeUpdate', this.size);
-  },
-  updated() {
-    console.log('updated', this.size);
-  },
-  beforeCreate() {
-    console.log('beforeCreate');
-    console.log('Estado:', this.size);
-    console.log('DOM:', this.$el);
-  },
-  created() {
-    console.log('created');
-    console.log('Estado:', this.size);
-    console.log('DOM:', this.$el);
-  },
-  beforeMount() {
-    console.log('beforeMount');
-    console.log('Estado:', this.size);
-    console.log('DOM:', this.$el);
-  },
-  mounted() {
-    console.log('mounted');
-    console.log('Estado:', this.size);
-    console.log('DOM:', this.$el);
-    window.addEventListener('resize', this.resize);
-  },
-  beforeUnmount() {
-    // Destruir as nossas libs
-    // Eventos
-    // Listeners
-    console.log('beforeUnmount');
-    window.removeEventListener('resize', this.resize);
-  },
-  unmounted() {
-    console.log('unmounted');
-  },
-  methods: {
-    resize($evt: Event) {
-      console.log($evt);
-      this.size = `${window.innerWidth} x ${window.innerHeight}`;
-    },
-  },
+// Criação
+//    Preparar o componente
+//    Ajax, inicializar algumas variáveis
+//    Não tem acesso ao template (DOM)
+console.log('created');
+console.log('Estado:', size.value);
+console.log('DOM:', root.value);
+console.log('----------------------------------------------------');
+
+// Montagem
+//    Inicializar um lib externa (new Lib())
+//    Precisa de acesso ao template (DOM)
+//    Tem acesso ao template (DOM)
+onBeforeMount(() => {
+  console.log('beforeMount');
+  console.log('Estado:', size.value);
+  console.log('DOM:', root.value);
+  console.log('----------------------------------------------------');
 });
+
+onMounted(() => {
+  console.log('mounted');
+  console.log('Estado:', size.value);
+  console.log('DOM:', root.value);
+  window.addEventListener('resize', resize);
+  console.log('----------------------------------------------------');
+});
+
+// Atualização
+//    Debug
+//    Update
+onBeforeUpdate(() => {
+  console.log('beforeUpdate');
+  console.log('Estado:', size.value);
+  console.log('DOM:', root.value);
+  console.log('----------------------------------------------------');
+});
+
+onUpdated(() => {
+  console.log('updated');
+  console.log('Estado:', size.value);
+  console.log('DOM:', root.value);
+  console.log('----------------------------------------------------');
+});
+
+// Desmontagem
+//    Remover tudo o que for necessário (lib->destroy())
+//    para liberar memória
+onBeforeUnmount(() => {
+  // Destruir as nossas libs
+  // Eventos
+  // Listeners
+  console.log('beforeUnmount');
+  console.log('Estado:', size.value);
+  console.log('DOM:', root.value);
+  window.removeEventListener('resize', resize);
+  console.log('----------------------------------------------------');
+});
+
+onUnmounted(() => {
+  console.log('unmounted');
+  console.log('Estado:', size.value);
+  console.log('DOM:', root.value);
+  console.log('----------------------------------------------------');
+});
+
 </script>
+
+<template>
+  <header class="header" ref="root">{{ size }}</header>
+</template>
 
 <style scoped>
 .header {
